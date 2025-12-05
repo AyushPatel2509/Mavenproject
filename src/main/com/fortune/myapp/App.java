60820e9fbd11884287ef1be12ec4d86a78bc6278
ayush <ayushrpatel2025@gmail.com>
2025-12-05 05:15:31 +0000

    cm1

---
+++ src/main/com/fortune/myapp/App.java
@@ -0,0 +1,36 @@
+  package com.fortune.myapp; 
+  
+  
+  /** 
+  
+   * Hello Ayush! 
+  
+   */ 
+  
+  public class App { 
+  
+   
+  
+      private static final String MESSAGE = "Hello Ayush!"; 
+  
+   
+  
+      public App() {} 
+  
+   
+  
+      public static void main(String[] args) { 
+  
+          System.out.println(MESSAGE); 
+  
+      } 
+  
+   
+  
+      public String getMessage() { 
+  
+          return MESSAGE; 
+  
+      } 
+  
+  } 
